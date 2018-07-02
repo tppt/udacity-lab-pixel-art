02c756fc42ecbc940c06ce8668a22799d388b70e
Thomas Travis <tptravis@gmail.com>
2018-07-01 21:05:45 -0400

    added functionality to build grid

--- designs.js
+++ designs.js
@@ -2,8 +2,25 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
+document.getElementById('sizePicker').addEventListener('submit', function (event) {
+  event.preventDefault();
+  makeGrid();
+});
 
 function makeGrid() {
-    // Your code goes here!
+  const height = document.getElementById('inputHeight').value;
+  const width = document.getElementById('inputWidth').value;
+
+  const table = document.getElementById('pixelCanvas');
+  //Clear the table before adding rows/cols to it
+  table.innerHTML = '';
+
+  for (let row = 0; row < height; row++) {
+    const tr = table.insertRow(row);
+    
+    for (let col = 0; col < width; col++) {
+      tr.insertCell(col);
+    }
   }
+}
   
